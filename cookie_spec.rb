4cd86e1d999c99c2818f0dbb3701cbe423f0e9f5
IevaVis <ieva.visniauskiene@gmail.com>
2018-05-17 21:34:26 +0800

    rspec testing

--- cookie_spec.rb
+++ cookie_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rspec'
 
 require_relative 'cookie'
@@ -23,49 +22,64 @@ describe Cookie do
   end
 
   describe "#type" do
-    it "returns the type of the cookie"
-
+    it "returns the type of the cookie" do
+      expect(cookie.type).to eq("peanut butter")
+    end
   end
 
   describe "#bake!" do
-    it "requires an integer time argument"
+    it "requires an integer time argument" do
+      expect{ cookie.bake!("c") }.to raise_error(TypeError)
 
+end
 
-    it "returns the cookie object"
+    it "returns the cookie object" do
+      expect(cookie.bake!(10)).to be_a_instance_of(cookie.class)
+    
 
-
+end
     it "changes the status of the cookie when given enough time" do
       expect { cookie.bake!(10) }.to change(cookie, :status)
     end
   end
 
   describe "#status" do
-    it "returns the cookie's current status"
+    it "returns the cookie's current status" do
+      expect(cookie.status).to eq(:doughy)
 
-
+end
     context "when unbaked" do
-      it "is `:doughy`"
-
+      it "is `:doughy`" do
     end
+  end
 
     context "when baked for less than 7 minutes" do
-      it "is `:doughy`"
+      it "is `:doughy`" do
+        expect(cookie.bake!(6).status).to eq(:doughy)
 
     end
+  end
 
     context "when baked for at least 7 but less than 10 minutes" do
-      it "is `:almost_ready`"
+      it "is `:almost_ready`" do
+        expect(cookie.bake!(9).status).to eq(:almost_ready)
+
+    end
 
     end
 
     context "when baked for at least 10 but less than 12 minutes" do
-      it "is `:ready`"
+      it "is `:ready`" do
+        expect(cookie.bake!(11).status).to eq(:ready)
+    end
 
     end
 
     context "when baked for at least 12 minutes" do
-      it "is `:burned`"
+      it "is `:burned`" do
+        expect(cookie.bake!(12).status).to eq(:burned)
 
     end
   end
+ end
 end
